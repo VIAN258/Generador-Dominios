1a103e9b690583a6bdb2ae69044091ae719d5c8c
VIAN258 <angelmorales891@gmail.com>
2022-12-03 17:03:00 +0000

    first

--- src/app.js
+++ src/app.js
@@ -7,5 +7,6 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoum = ["The", "Our", "Us", "They", "He", "She"];
+  let = ["The", "Our", "Us", "They", "He", "She"];
 };
