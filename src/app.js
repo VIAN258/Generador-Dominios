d8f2aaf52e17d72154e44b3f694eba141eddefd8
VIAN258 <angelmorales891@gmail.com>
2022-12-04 02:00:36 +0000

    second

--- src/app.js
+++ src/app.js
@@ -7,6 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let pronoum = ["The", "Our", "Us", "They", "He", "She"];
-  let = ["The", "Our", "Us", "They", "He", "She"];
+let pronoum = ['The', 'Our','They', 'He', 'She'];
+let adjetivo = ['great', 'big', 'strong', 'weak', 'shame', 'furious', 'angry'];
+let noun = ['angel', 'ana', 'antonia', 'maibel', 'lenis', 'nelson', 'maria', 'katy'];
+let extension = ['.com', '.net', '.cl', '.co', '.ve'];
+
+  for (let i=0; i<pronoum.length; i++){
+    for (let j=0; j<adjetivo.length; j++){
+      for (let k=0; k<noun.length; k++){
+        for (let l=0; l<extension.length; l++){
+          console.log(pronoum[i]+''+adjetivo[j]+''+noun[k]+''+extension[l]);
+          
+        }
+      }
+    }
+  }
 };
